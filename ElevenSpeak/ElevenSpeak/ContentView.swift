0e3b07ac7f62f6ae664c8f9a14248540ab7bd2e1
Bogdan Bystritskiy <q80061@gmail.com>
2023-06-02 14:41:25 +0300

    Initial project

---
+++ ElevenSpeak/ElevenSpeak/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ElevenSpeak
+//
+//  Created by Bogdan Bystritskiy on 02.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ElevenSpeak/ElevenSpeak/ElevenSpeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElevenSpeakApp.swift
+//  ElevenSpeak
+//
+//  Created by Bogdan Bystritskiy on 02.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ElevenSpeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
