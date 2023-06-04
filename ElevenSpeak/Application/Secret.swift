5728a6baaac3f1b7564dd3dd989ee1442b62a2b0
Bogdan Bystritskiy <q80061@gmail.com>
2023-06-04 14:55:36 +0300

    Fix

--- ElevenSpeak/Application/Secret.swift
+++ ElevenSpeak/Application/Secret.swift
@@ -2,7 +2,7 @@
 // ElevenSpeak. Created by Bogdan Bystritskiy.
 
 enum Secret {
-    static let whisperKey = "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"
-    static let gptKey = "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"
-    static let elevenlabsKey = "aad45edd95d9247ebce10502e9b958ea"
+    static let whisperKey = "YOUR_TOKEN_HERE"
+    static let gptKey = "YOUR_TOKEN_HERE"
+    static let elevenlabsKey = "YOUR_TOKEN_HERE"
 }
