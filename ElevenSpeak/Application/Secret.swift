8567be1d869f8d6249ed0abe8b7e3d7c4f11ec4f
Bogdan Bystritskiy <q80061@gmail.com>
2023-06-04 14:17:47 +0300

    Re-folder strcture

---
+++ ElevenSpeak/Application/Secret.swift
@@ -0,0 +1,8 @@
+// Secret.swift
+// ElevenSpeak. Created by Bogdan Bystritskiy.
+
+enum Secret {
+    static let whisperKey = "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"
+    static let gptKey = "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"
+    static let elevenlabsKey = "aad45edd95d9247ebce10502e9b958ea"
+}
--- ElevenSpeak/ElevenSpeak/Application/Secret.swift
+++
@@ -1,8 +0,0 @@
-// Secret.swift
-// ElevenSpeak. Created by Bogdan Bystritskiy.
-
-enum Secret {
-    static let whisperKey = "YOUR_TOKEN_HERE"
-    static let gptKey = "YOUR_TOKEN_HERE"
-    static let elevenlabsKey = "YOUR_TOKEN_HERE"
-}
